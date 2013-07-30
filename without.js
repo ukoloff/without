e7bae9756d2139b8435cc80d025e7784be58efcd
Stas Ukolov <ukoloff@gmail.com>
2013-07-30 12:31:51 +0600

    Add standard tags

--- without.js
+++ without.js
@@ -6,8 +6,10 @@ withOut=(function()
 {
   var
     html='',
-    options= {autoesc: true},
+    options={autoesc: true},
     entities={amp: '&', lt: '<', gt: '>', quot: '"'},
+    nTags='a abbr acronym address applet article aside audio b bdo big blockquote body button canvas caption center cite code colgroup command datalist dd del details dfn dir div dl dt em embed fieldset figcaption figure font footer form frameset h1 h2 h3 h4 h5 h6 head header hgroup html i iframe ins keygen kbd label legend li map mark menu meter nav noframes noscript object ol optgroup option output p pre progress q rp rt ruby s samp script section select small source span strike strong style sub summary sup table tbody td textarea tfoot th thead time title tr tt u ul var video wbr xmp'.split(' '),
+    eTags='area base basefont br col frame hr img input link meta param'.split(' '),
     scope={}
 
   function h(s)
@@ -74,6 +76,35 @@ withOut=(function()
     }
   }
 
+  function makeTag(name, empty)
+  {
+    return function(){tag(arguments)}
+    function attrs(a)
+    {
+      for(var k in a)
+      {
+        v=a[k]
+        if((null==v) || (false===v)) continue
+        fragments([' ', k])
+        if(true!==v)
+        {
+          html+='="'
+          fragments([v])
+          html+='"'
+        }
+      }
+    }
+    function tag(a)
+    {
+      html+='<'+name;
+      if('object'==typeof a[0]){attrs(a[0]); a=[].slice.call(a, 1)}
+      html+='>'
+      if(empty && a.length) throw "<"+name+"> must have no content!"
+      if(empty) return
+      children(a)
+      html+="</"+name+">"
+    }
+  }
 
   var fragments=makeLists()
   var children=makeLists(true)
@@ -81,6 +112,9 @@ withOut=(function()
   scope.print=function(){fragments(arguments)}
   scope.raw=makeRaw();
   scope.esc=makeEsc();
+  for(var i in nTags) scope[nTags[i]]=makeTag(nTags[i])
+  for(var i in eTags) scope[eTags[i]]=makeTag(eTags[i], true)
+
 
   return {
     dump: function(){ console.log(html); html='' },
