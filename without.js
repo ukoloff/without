a3ebe6cb8f7452250d78f994173809d63719ac96
Stas Ukolov <ukoloff@gmail.com>
2013-07-30 10:49:02 +0600

    Print lists of values

--- without.js
+++ without.js
@@ -2,13 +2,50 @@
 // without.js - CoffeScript template engine with lexical scoping
 //
 
+withOut=(function()
+{
+  var
+    html='',
+    options= {autoesc: true},
+    entities={amp: '&', lt: '<', gt: '>', quot: '"'}
 
+  function h(s)
+  {
+   s=''+s
+   for(var i in entities)
+     s=s.split(entities[i]).join("&"+i+";")
+   return s
+  }
 
+  function makeLists(nested)
+  {
+    return function list(a)
+    {
+      autoesc=options.autoesc
+      for(var i in a)
+      {
+        var e=a[i]
+        if(null==e) continue;
+        if(nested && ('function'==typeof e))
+        {
+         e()
+         continue
+        }
+        html+=autoesc? h(e):e
+      }
+    }
+  }
 
+  var fragments=makeLists()
+  var children=makeLists(true)
 
-
-
-
+  return {
+    dump: function(){ console.log(html); html='' },
+    h: h,
+    fragments: fragments, 
+    children: children
+  }
+})()
 
 
 //--[EOF]------------------------------------------------------------
