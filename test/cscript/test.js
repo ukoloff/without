b7905389457792f7036d8d7cc63485595fc8169d
Stas Ukolov <ukoloff@gmail.com>
2013-12-27 14:57:09 +0600

    coffeeLoad

--- test/cscript/test.js
+++ test/cscript/test.js
@@ -7,15 +7,39 @@ $={
   fso: new ActiveXObject("Scripting.FileSystemObject")
 }
 
-jsLoad('withOut.js')
-WScript.Echo(withOut)
+coffeeLoad('test/cscript/a.coffee')
 
-function jsLoad(name)
+function jsLoad(name, drop)
 {
   f=$.fso.OpenTextFile(name)
   fn=new Function(f.ReadAll())
   f.Close()
+  if(drop) $.fso.DeleteFile(name)
   fn()
+}
+
+function rnd(N)
+{
+ for(var S=''; S.length<(N||12); )
+ {
+  var n=Math.floor(62*Math.random())
+  S+=String.fromCharCode('Aa0'.charCodeAt(n/26)+n%26)
+ }
+ return S
+}
+
+function tmp()
+{
+  var t=$.sh.ExpandEnvironmentStrings('%TEMP%/')
+  do var n=t+rnd(); while($.fso.FileExists(n))
+  return n
+}
+
+function coffeeLoad(name)
+{
+  t=tmp()
+  $.sh.Run('coffee -cp "'+name+'" >"'+t+'"', 0, true)
+  jsLoad(t, true)
 }
 
 //--[EOF]------------------------------------------------------------
