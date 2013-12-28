2495dff21ff9b91bd366d4e1a49f5802b5099c51
Stas Ukolov <ukoloff@gmail.com>
2013-12-28 20:45:05 +0600

    List its

--- test/www/test.js
+++ test/www/test.js
@@ -1,8 +1,24 @@
+GLOBAL={JST: JST={}}
+
 function require(path)
 {
 }
 
+htmlEntities={'&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;'}
+
+function h(s)
+{
+  return String(s).replace(/[&<>"]/g, function(e){return htmlEntities[e]})
+}
+
 function describe(name, fn)
 {
-  document.writeln('<li>', name, '</li>')
+  document.writeln('<li>', h(name), '</li><ul>')
+  fn()
+  document.writeln('</ul>')
+}
+
+function it(name, fn)
+{
+  document.writeln('<li>', h(name), '</li>')
 }
