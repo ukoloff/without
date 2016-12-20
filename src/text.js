6feca7f6637be0995475af9b5cda0578e650b2a6
Stas Ukolov <ukoloff@gmail.com>
2016-12-20 09:05:47 +0500

    Add print wrapper

---
+++ src/text.js
@@ -0,0 +1,4 @@
+function text()
+{
+  print(arguments)
+}
--- src/zscope.js
+++ src/zscope.js
@@ -1,5 +1,6 @@
 var scope = {
-  print: function() { print(arguments) },
+  print: text,
+  text: text,
   raw: function() { raw(arguments) },
   notag: function() { noTag(arguments) },
   coffeescript: function() { coffeeScript(arguments) },
@@ -8,8 +9,6 @@ var scope = {
   tag: adhocTag(),
   $var: makeTag('var')
 }
-
-scope.text = scope.print
 
 for(var i in nTags) scope[nTags[i]] = makeTag(nTags[i])
 for(var i in eTags) scope[eTags[i]] = makeTag(eTags[i], true)
