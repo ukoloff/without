a36676204a97edeed8441c19f264eddffe6ccc56
Stas Ukolov <ukoloff@gmail.com>
2017-01-03 14:56:35 +0500

    Allow embed in <script>

--- src/coffee.js
+++ src/coffee.js
@@ -3,5 +3,6 @@ function coffeeScript(a)
 {
   if(1 != a.length || 'function' != typeof a[0])
     throw SyntaxError('Usage: coffeescript -> code')
-  html += '<script><!--\n(' + a[0].toString() + ')()\n//-->\n</script>';
+  html += '<script><!--\n(' + a[0].toString() + ')()\n//-->\n</'
+  html += 'script>'
 }
