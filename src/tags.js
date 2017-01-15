3d2a0c5f8096aa14cafa9d9aecc388736887f2ca
Stas Ukolov <ukoloff@gmail.com>
2017-01-15 11:56:29 +0500

    Always split long tag list

--- src/tags.js
+++ src/tags.js
@@ -1,13 +1,13 @@
 // List of regular tags
 function nTags()
 {
-  return 'a abbr acronym address applet article aside audio ' +
-  'b bdo big blockquote body button canvas caption center cite code colgroup command ' +
-  'datalist dd del details dfn dir div dl dt em embed ' +
-  'fieldset figcaption figure font footer form frameset ' +
-  'h1 h2 h3 h4 h5 h6 head header hgroup html i iframe ins keygen kbd ' +
-  'label legend li map mark menu meter nav noframes noscript ' +
-  'object ol optgroup option output p pre progress q rp rt ruby ' +
-  's samp script section select small source span strike strong style sub summary sup ' +
-  'table tbody td textarea tfoot th thead time title tr tt u ul video wbr xmp'
+  var s = 'a abbr acronym address applet article aside audio '
+  s += 'b bdo big blockquote body button canvas caption center cite code colgroup command '
+  s += 'datalist dd del details dfn dir div dl dt em embed '
+  s += 'fieldset figcaption figure font footer form frameset '
+  s += 'h1 h2 h3 h4 h5 h6 head header hgroup html i iframe ins keygen kbd '
+  s += 'label legend li map mark menu meter nav noframes noscript '
+  s += 'object ol optgroup option output p pre progress q rp rt ruby '
+  s += 's samp script section select small source span strike strong style sub summary sup '
+  return s + 'table tbody td textarea tfoot th thead time title tr tt u ul video wbr xmp'
 }
