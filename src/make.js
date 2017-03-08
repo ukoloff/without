aa88d73e647178d37976d2af50763e0481e75995
Stas Ukolov <ukoloff@gmail.com>
2017-03-08 21:29:08 +0500

    Some speed up for MSIE6

--- src/make.js
+++ src/make.js
@@ -5,29 +5,10 @@ function makeTag(name, empty)
 
   function Tag() { tag(arguments) }
 
-  // Output single attribute
-  function attr(k, v)
-  {
-    if(null == v || false === v) return
-    html += ' ' + h(k)
-    if(true !== v)
-      html += '="' + h(v) + '"'
-  }
-
-  // Output data-* attribute(s)
-  function nest(prefix, obj)
-  {
-    for(var k in obj)
-      if('object' == typeof obj[k])
-        nest(prefix + k + '-', obj[k])
-      else
-        attr(prefix + k, obj[k])
-  }
-
   // Output full tag
   function tag(a)
   {
-    html += '<' + name
+    var me = '<' + name
     var at = a[0]
     if('object' == typeof at)
     {
@@ -38,12 +19,31 @@ function makeTag(name, empty)
          attr(k, at[k])
      a = shift(a)
     }
-    html += '>'
+    html += me + '>'
     if(empty && a.length)
       throw SyntaxError("<" + name + "> must have no content!")
     if(empty)
       return
     children(a)
     html += "</" + name + ">"
+
+    // Output single attribute
+    function attr(k, v)
+    {
+      if(null == v || false === v) return
+      me += ' ' + h(k)
+      if(true !== v)
+        me += '="' + h(v) + '"'
+    }
+
+    // Output data-* attribute(s)
+    function nest(prefix, obj)
+    {
+      for(var k in obj)
+        if('object' == typeof obj[k])
+          nest(prefix + k + '-', obj[k])
+        else
+          attr(prefix + k, obj[k])
+    }
   }
 }
