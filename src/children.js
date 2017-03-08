98293003c8450a9192043228da4a92b83642c7bd
Stas Ukolov <ukoloff@gmail.com>
2017-03-08 21:45:42 +0500

    Even more speed up

--- src/children.js
+++ src/children.js
@@ -1,13 +1,20 @@
 // Output text & inner tags
 function children(a)
 {
-  var e, len = a.length
+  var e, len = a.length, prev
   for(var i = 0; i < len; i++)
-  {
-    if(null == (e = a[i])) continue;
-    if('function' == typeof e)
-      e.call(_this)
-    else
-      html += h(e)
-  }
+    try
+    {
+      prev = html
+      html = ''
+      if(null == (e = a[i])) continue;
+      if('function' == typeof e)
+        e.call(_this)
+      else
+        html = h(e)
+    }
+    finally
+    {
+      html = prev + html
+    }
 }
